e6dd5ba9ed1874eb562f83531f74f471a388d85a
harkirat <harkiratsvirk@gmail.com>
2023-02-12 12:05:22 -0500

    redesigning build system

--- scripts/buildsystem.py
+++ scripts/buildsystem.py
@@ -22,7 +22,8 @@ class BuildContainer:
         import os
         self.build_path = dir_list.dir_repo
         self.src_path = dir_list.dir_download
-        self.log_path = os.path.join(dir_list.dir_log, 'build')
+        self.log_path = dir_list.dir_log
+        self.buildlog_path = os.path.join(dir_list.dir_log, 'build')
         self.conf_path = dir_list.dir_config
 
         try:
@@ -38,12 +39,16 @@ class BuildContainer:
                 Print("Building Docker Environment...")
                 image, build_logs = self.client.images.build(path=dir_list.dir_config, tag='athenalinux:build',
                                                              nocache=True, rm=True)
-                for chunk in build_logs:
-                    if 'stream' in chunk:
-                        for line in chunk['stream'].splitlines():
-                            pass
-                            # Print(line)
-
+                Print(f"Docker Environment Built - {image.tags}")
+                try:
+                    with open(os.path.join(self.log_path, 'docker_build.log'), 'w') as fh:
+                        for chunk in build_logs:
+                            if 'stream' in chunk:
+                                for line in chunk['stream'].splitlines():
+                                    fh.write(line + '\n')
+                except (FileNotFoundError, PermissionError) as e:
+                    Print(f"Error: {e}")
+                    exit(1)
             self.image = image
 
         except docker.errors.APIError as e:
@@ -53,11 +58,10 @@ class BuildContainer:
     def container_execute(self, command: str):
 
         try:
-            # client.images.get("master_image").save("master_image.tar")
             cmd_str = 'mkdir /build; cd /source; ls -al; apt -y install debhelper-compat zlib1g-dev mawk;' \
                       ' dpkg-source -x libpng1.6_1.6.37-3.dsc /build/libpng; cd /build/libpng; dpkg-checkbuilddeps;' \
                       ' dpkg-buildpackage -a amd64 -us -uc -J; cd /build; cp *.deb /source/'
-            container = self.client.containers.run("athenabuild", command=f"/bin/bash -c '{cmd_str}'",
+            container = self.client.containers.run("athenalinux:build", command=f"/bin/bash -c '{cmd_str}'",
                                                    detach=True, auto_remove=False,
                                                    volumes={self.src_path: {'bind': '/source', 'mode': 'rw'}})
             for line in container.logs(stream=True):
@@ -66,8 +70,6 @@ class BuildContainer:
             # for line in container.logs(stream=True):
             #    Print(line.decode("utf-8"))
 
-            # Copy the file from the container to the host file system
-            # container.copy("/output.txt")
 
             # Save the file to the host file system
             # with open("output.txt", "wb") as f:
