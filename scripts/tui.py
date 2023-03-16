a792f1e67c7cb01e34886c74ba06f8b51c723950
harkirat <harkiratsvirk@gmail.com>
2023-03-16 11:51:41 +0530

    removed empty licence file

--- scripts/tui.py
+++ scripts/tui.py
@@ -1,1 +1,7 @@
+#  Copyright (c) 2023. Harkirat S Virk <harkiratsvirk@gmail.com>
+#
+#  This program comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+#  This is free software, and you are welcome to redistribute it under certain conditions; see COPYING for details.
+
 import curses
+from curses import wrapper
